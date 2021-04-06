71989f7b254e13385f8ac7fe525ca4e969f981ef
czebos <conrad.zborowski@gmail.com>
2021-04-06 17:10:50 -0400

    Basic GamePlay completed

---
+++ game.py
@@ -0,0 +1,53 @@
+from constants import EMPTY, PLAYER_ONE, PLAYER_TWO
+from game_over import CheckGameOver
+
+"""
+We chose to represent Connect 4 as a class, becase
+there are a lot of variables that should be stored 
+for a given game. It has the methods init, makemove
+and and switchTurns.
+"""
+class Connect4:
+
+    """
+    This function just initiates all of the starting
+    data for the Connect4 Game
+    """
+    def __init__(self, width, height):
+        self.board = [[EMPTY for _ in range(width)] for _ in range(height)]
+        self.turn = PLAYER_ONE
+        self.width = width
+        self.height = height
+
+    """
+    This function makes the move if its valid.
+    It returns the winner if there is one.
+    """
+    def makeMove(self, col):
+        # If outside the boundaries of width
+        if col < 0 or col >= self.width:
+            print("Not a valid move")
+            return EMPTY
+        else:
+            # If col filled up
+            if self.board[0][col] != EMPTY:
+                print("Not a valid move")
+                return EMPTY
+            # Find the first time a slot is open
+            top = 0
+            while top < self.height and self.board[top][col] == EMPTY:
+                top += 1
+            top -= 1
+            self.board[top][col] = self.turn
+            self.switchTurns()
+            return CheckGameOver(self.board)
+
+    """
+    This function switches the current players move.
+    """
+    def switchTurns(self):
+        if self.turn == PLAYER_ONE:
+            self.turn = PLAYER_TWO
+        else:
+            self.turn = PLAYER_ONE
+
--- game_over.py
+++ game_over.py
@@ -1,19 +1,30 @@
 from constants import EMPTY, PLAYER_ONE, PLAYER_TWO
 
+"""
+This checks if there is a current winner for the game
+board and returns it.
+"""
 def CheckGameOver(board):
-    for i in range(len(board)):
+    for i in range(len(board)): # for each space
         for j in range(len(board[0])):
-            winner = CheckDirections(board,i,j) 
+            winner = CheckDirections(board,i,j) # check if theres 4 in a row in any direciton
             if winner != EMPTY:
                 return winner
     return EMPTY
 
-
+"""
+This function returns the winner if there is one.
+It takes in a board, and a given x,y representing
+a space. It then checked every direction from that 
+space for a connect 4.
+"""
 def CheckDirections(board, x, y):
     color = board[x][y]
+    # No winner
     if color == EMPTY:
         return EMPTY
     dir = [-1,0,1]
+    # check every direction, up, left right down and combinations of them
     for i in dir:
         for j in dir:
             currX = x
@@ -21,22 +32,13 @@ def CheckDirections(board, x, y):
             count = 0
             if i == 0 and j == 0:
                 continue
+            # if its the same piece as before, and in bounds, keep going that direction.
             while currX >= 0 and currX < len(board) and currY >=0 and currY < len(board[1]) and board[currX][currY] == color:
                 count += 1
                 currX += i
                 currY += j
+            # if thers a connect4
             if count >= 4:
                 return color
 
     return EMPTY
-
-def printBoard(board):
-    for i in board:
-        string = ""
-        for j in i:
-            string += str(j)
-        print(string)
-
-board = [[1,0,0,0,0], [1,1,0,1,1], [1,0,0,0,0], [1,1,0,1,1], [2,2,0,2,2]]
-printBoard(board)
-print(CheckGameOver(board))
